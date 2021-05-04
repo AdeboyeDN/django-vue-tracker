bd94852e05826a42e6ed3ace4b960cf829d8d76c
daniel <adeboyed93@gmail.com>
2021-05-04 05:55:21 +0100

    'new-vue'

---
+++ tracker_vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Headers />
+  
+</template>
+
+<script>
+import Headers from './components/Headers.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Headers
+  }
+}
+</script>
+
+
---
+++ tracker_vue/src/components/Form.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+     name : Form,
+     props : String,
+     data() {
+
+     },
+     methods : {
+          
+     }
+}
+</script>
+
